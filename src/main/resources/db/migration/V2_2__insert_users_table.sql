9d2a2055a59870d9b422cc622227b40c956e8de1
Amadoumacka224 <mackaldine@gmail.com>
2025-08-04 22:57:48 +0200

    modification du fichier properties

--- src/main/resources/db/migration/V2_2__insert_users_table.sql
+++
@@ -1,7 +0,0 @@
-
--- Données de la table `users`
---
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `enabled`) VALUES
-                                                                                                      (1, 'admin', '$2a$10$GRLdNn3.b.p2y91c.Q3g6uJd.pD.E.Yg1.yYg1.yYg1.yYg1.yYg', 'admin@gestionpro.com', 'Admin', 'Istrateur', 1),
-                                                                                                      (2, 'jdupont', '$2a$10$GRLdNn3.b.p2y91c.Q3g6uJd.pD.E.Yg1.yYg1.yYg1.yYg1.yYg', 'j.dupont@gestionpro.com', 'Jean', 'Dupont', 1),
-                                                                                                      (3, 'amartin', '$2a$10$GRLdNn3.b.p2y91c.Q3g6uJd.pD.E.Yg1.yYg1.yYg1.yYg1.yYg', 'a.martin@gestionpro.com', 'Alice', 'Martin', 1);
