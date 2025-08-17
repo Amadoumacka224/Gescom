83d8b7d15643e05c1d91b84a4d6e3757030e5de0
Amadoumacka224 <mackaldine@gmail.com>
2025-08-17 16:02:34 +0200

    Mise à jours apres suppréssion

--- set-jdk17.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Configuration JDK 17...
-set JAVA_HOME=C:\Program Files\Java\jdk-17
-set PATH=C:\Program Files\Java\jdk-17\bin;%PATH%
-echo JDK 17 configuré avec succès
-java -version
